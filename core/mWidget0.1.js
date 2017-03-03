8f2fcd515f663902679c4d059c6b02a4ec579e2a
miguel <miguel@miguels-MBP.mty.base22.com>
2017-03-03 13:55:12 -0600

    support sub templetes

--- core/mWidget0.1.js
+++ core/mWidget0.1.js
@@ -1,7 +1,10 @@
 (function ($, window, undefined) {
     $.mWidget = function (params) {
-        if (!(params.dataAjax || params.data) || !(params.tplAjax || params.tpl) || !params.target) {
-            throw "mWidget parameters object requires a 'tpl'/'tplAjax', a 'data'/'dataAjax' and a 'target' member";
+        console.log('call to mWidget');
+        console.log('data?' + ((params.dataAjax || params.data) != undefined));
+        console.log('tpl?' + ((params.tplAjax || params.tpl) != undefined));
+        if (!(params.dataAjax || params.data) || !(params.tplAjax || params.tpl)) {
+            throw "mWidget parameters object requires a 'tpl'/'tplAjax' and a 'data'/'dataAjax' %o", params;
         }
         if (!params.data) {
             // use the dataAjax object to get the data and then call $.mWidget again.
@@ -20,10 +23,21 @@
             $.ajax(params.tplAjax);
         }
         else {
-            // for each data element, apply the customHandler, default customHandler is 'data => data'. 
+            var res = '';
             $.each((params.customHandler || (function (data) { return data; }))(params.data), function (i, entry) {
-                $(params.target).append(params.tpl.replace(/{{[_a-zA-Z][_a-zA-Z0-9]*}}/g, function (request) { return entry[request.slice(2, -2)]; }));
+                var part = params.tpl.replace(/\[\[[^\]\]]*\]\]/g, function (arrayRequest) {
+                    return (entry[arrayRequest.substring(2, arrayRequest.indexOf('\n'))] ?
+                        $.mWidget({
+                            tpl: arrayRequest.slice(arrayRequest.indexOf('\n'), -2),
+                            data: entry[arrayRequest.substring(2, arrayRequest.indexOf('\n'))]
+                        }) :
+                        '');
+                });
+                res += part.replace(/{{[_a-zA-Z][_a-zA-Z0-9]*}}/g, function (request) { return entry[request.slice(2, -2)]; });
             });
+            if (params.target)
+                $(params.target).append(res);
+            return res;
         }
     };
 })(jQuery, window);
--- core/mWidget0.1.ts
+++ core/mWidget0.1.ts
@@ -1,7 +1,10 @@
 (function ($, window, undefined) {
     $.mWidget = function (params) {
-        if (!(params.dataAjax || params.data) || !(params.tplAjax || params.tpl) || !params.target) {
-            throw "mWidget parameters object requires a 'tpl'/'tplAjax', a 'data'/'dataAjax' and a 'target' member";
+    	console.log('call to mWidget');
+    	console.log('data?' + ((params.dataAjax || params.data) != undefined));
+    	console.log('tpl?' + ((params.tplAjax || params.tpl) != undefined));
+        if (!(params.dataAjax || params.data) || !(params.tplAjax || params.tpl) ) {
+            throw "mWidget parameters object requires a 'tpl'/'tplAjax' and a 'data'/'dataAjax' %o", params;
         }
 		if (!params.data) {
 			// use the dataAjax object to get the data and then call $.mWidget again.
@@ -20,10 +23,22 @@
 			$.ajax(params.tplAjax);
 		}
 		else {
-			// for each data element, apply the customHandler, default customHandler is 'data => data'. 
-			$.each( (params.customHandler || (data => data))(params.data), (i, entry) => { //replace anything that needs ot be replaced in the tpl, and append it to the 'target'.
-				$(params.target).append(params.tpl.replace(/{{[_a-zA-Z][_a-zA-Z0-9]*}}/g, request => entry[request.slice(2, -2)]));
+			var res = '';
+			$.each( (params.customHandler || (data => data))(params.data), (i, entry) => { // for each data element, apply the customHandler, default customHandler is 'data => data'. 				
+				var part = params.tpl.replace(/\[\[[^\]\]]*\]\]/g, arrayRequest =>
+					(entry[arrayRequest.substring(2, arrayRequest.indexOf('\n'))]?	
+						$.mWidget({ // use mWidget to compute inner tpl
+							tpl: arrayRequest.slice(arrayRequest.indexOf('\n'), -2),
+							data: entry[arrayRequest.substring(2, arrayRequest.indexOf('\n'))]
+						}) : 
+						''
+					)
+				);
+				res += part.replace(/{{[_a-zA-Z][_a-zA-Z0-9]*}}/g, request => entry[request.slice(2, -2)] );
 			});
+			if(params.target)
+				$(params.target).append(res);
+			return res;
 		}
 	}
 })(jQuery, window);
